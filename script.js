f649dae96500d4fa54bc93922e3adb8ef295b9f3
Xueer <xueerzhang@users.noreply.github.com>
2019-11-29 17:36:22 +0800

    test intersection observer on .case-study

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// const options = {
+//   //root: null,
+//   //rootMargin: '0px',
+//   threshold: 0.5
+// }
 
+function callback (entries, observer) {
+  console.log(observer);
+
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+}
+
+let observer = new IntersectionObserver(callback, {threshold: 0.5});
+observer.observe(document.querySelector('.case-study'));
