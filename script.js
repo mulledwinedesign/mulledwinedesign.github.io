bc337d592ae30c91f97c0c3f78f1c2cc76281178
Xueer <xueerzhang@users.noreply.github.com>
2019-11-29 20:22:00 +0800

    test intersection observer on .case-study

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.5
-}
+// const options = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 0.5
+// }
 
 function callback (entries, observer) {
   console.log(observer);
+
   entries.forEach(entry => {
+    console.log(entry);
+
     if (entry.isIntersecting == true && entry.intersectionRatio > 0.5) {
       console.log("intersection over 50%");
     }
-    console.log(entry);
   });
 }
 
-let observer = new IntersectionObserver(callback, options);
-observer.observe(document.querySelector('.case-study'));
+let observer = new IntersectionObserver(callback, {threshold: 0.5});
+observer.observe(document.querySelector('#test-target'));
