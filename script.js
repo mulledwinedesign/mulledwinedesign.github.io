548e120bb382efd3ffed1a59091bac80c3f6823c
Xueer <xueerzhang@users.noreply.github.com>
2019-11-30 17:15:09 +0800

    apply rootMargin

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// const options = {
-//   root: null,
-//   rootMargin: '0px',
-//   threshold: 0.5
-// }
+const options = {
+  // root: null,
+  rootMargin: '0px 0px 50% 0px',
+  // threshold: 0
+}
 
 function callback (entries, observer) {
   console.log(observer);
@@ -10,11 +10,11 @@ function callback (entries, observer) {
   entries.forEach(entry => {
     console.log(entry);
 
-    if (entry.isIntersecting == true && entry.intersectionRatio >= 0.5) {
-      console.log("intersection over 50%");
+    if (entry.isIntersecting == true && entry.intersectionRatio >= 0.3) {
+      console.log("intersection over 30%");
     }
   });
 }
 
-let observer = new IntersectionObserver(callback, {threshold: 0.5});
+let observer = new IntersectionObserver(callback, options);
 observer.observe(document.querySelector('#test-target'));
