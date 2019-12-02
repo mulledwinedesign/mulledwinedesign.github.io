e64977a4c9990f29a76008551439a02d02b7bc34
Xueer <xueerzhang@users.noreply.github.com>
2019-12-02 16:35:32 +0800

    simplify functions

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-function buildThresholdList (numSteps) {
-  let thresholds = [];
-
-  for (let i=1.0; i<=numSteps; i++) {
-    let ratio = i/numSteps;
-    thresholds.push(ratio);
-  }
-
-  thresholds.push(0);
-  return thresholds;
-}
+// function buildThresholdList (numSteps) {
+//   let thresholds = [];
+//
+//   for (let i=1.0; i<=numSteps; i++) {
+//     let ratio = i/numSteps;
+//     thresholds.push(ratio);
+//   }
+//
+//   thresholds.push(0);
+//   return thresholds;
+// }
 
 const options = {
   // root: null,
   // rootMargin: '0px 0px -50% 0px',
-  threshold: buildThresholdList(100)
+  // threshold: buildThresholdList(100)
+  threshold: {for (let i=0; i<=1.0; i+= 0.01) {threshold[0].push(i);}}
 }
-
-// for (let i=0; i<=1.0; i+= 0.01) {
-//   thresholdList[0].push(i);
-// }
 
 function callback (entries, observer) {
   // console.log(observer);
 
-  // entries.forEach(entry => {
-  //   console.log(entry);
-  //   if (entry.isIntersecting) {
-  //     console.log(entry.intersectionRatio);
-  //   }
-  // });
-
-  entries.forEach(function(entry){
+  entries.forEach(entry => {
     // console.log(entry);
     if (entry.isIntersecting) {
       console.log(entry.intersectionRatio);
@@ -38,7 +28,6 @@ function callback (entries, observer) {
       console.log("out of range");
     }
   });
-
 }
 
 let observer = new IntersectionObserver(callback, options);
