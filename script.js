46190b62fcb6b4976e449359403bd579ebba0248
Xueer <xueerzhang@users.noreply.github.com>
2019-12-02 16:12:33 +0800

    print ratio

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
+function buildThresholdList (numSteps) {
+  let thresholds = [];
+  let numSteps;
+
+  for (let i=1.0; i<=numSteps; i++) {
+    let ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
+
 const options = {
   // root: null,
-  rootMargin: '0px 0px -50% 0px',
-  // threshold: 0
+  // rootMargin: '0px 0px -50% 0px',
+  threshold: buildThresholdList(100)
 }
+
+// for (let i=0; i<=1.0; i+= 0.01) {
+//   thresholdList[0].push(i);
+// }
 
 function callback (entries, observer) {
   console.log(observer);
 
-  entries.forEach(entry => {
-    console.log(entry);
+  // entries.forEach(entry => {
+  //   console.log(entry);
+  //   if (entry.isIntersecting) {
+  //     console.log(entry.intersectionRatio);
+  //   }
+  // });
 
-    if (entry.isIntersecting == true && entry.intersectionRatio >= 0.3) {
-      console.log("intersection over 30%");
+  entries.forEach(function(entry){
+    console.log(entry);
+    if (entry.isIntersecting) {
+      console.log(entry.intersectionRatio);
     }
   });
+
 }
 
 let observer = new IntersectionObserver(callback, options);
