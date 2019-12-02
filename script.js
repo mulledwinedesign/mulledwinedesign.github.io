7cce6fb98a87482a780e9967d77e1b929eef4b23
Xueer <xueerzhang@users.noreply.github.com>
2019-12-02 17:00:33 +0800

    observe all '.cs-cloudrone p'

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// function buildThresholdList (numSteps) {
-//   let thresholds = [];
-//
-//   for (let i=1.0; i<=numSteps; i++) {
-//     let ratio = i/numSteps;
-//     thresholds.push(ratio);
-//   }
-//
-//   thresholds.push(0);
-//   return thresholds;
-// }
+function buildThresholdList (numSteps) {
+  let thresholds = [];
+
+  for (let i=1.0; i<=numSteps; i++) {
+    let ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
 
 const options = {
   // root: null,
   // rootMargin: '0px 0px -50% 0px',
-  // threshold: buildThresholdList(100)
-  threshold: thresholdList[] => {for (let i=0; i<=1.0; i+= 0.01) {thresholdList[0].push(i);}}
+  threshold: buildThresholdList(100)
 }
 
 function callback (entries, observer) {
@@ -31,4 +30,5 @@ function callback (entries, observer) {
 }
 
 let observer = new IntersectionObserver(callback, options);
-observer.observe(document.querySelector('#test-target'));
+// observer.observe(document.querySelector('#test-target'));
+document.querySelectorAll('.cs-cloudrone p').forEach(p => { observer.observe(p) });
