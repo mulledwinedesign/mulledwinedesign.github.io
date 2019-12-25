355cc75c36ec9b1defe5f185850404cd83019c67
Xueer <xueerzhang@users.noreply.github.com>
2019-12-25 11:11:05 +0800

    work out logic: if (甲) { if (乙) {A} else if (丙) {B} else {A} }

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function buildThresholdList (numSteps) {
 const options = {
   // root: null,
   // rootMargin: '0px 0px -50% 0px',
-  threshold: buildThresholdList(10)
+  threshold: buildThresholdList(100)
 }
 
 // const arr = [
@@ -31,45 +31,58 @@ const options = {
 // const obj = { b: "one", a: "two", a: "three" };
 // console.log(obj); // returns {b: "one", a: "three"}
 
-// ES6 offers some interesting built-in key-sorting during the creation of the object. Object keys are sorted only based on numerics/numeric-strings, all the other keys are pushed right after the numeric keys unsorted.
-// // object with numeric/numeric-string keys are sorted
-// const obj = { 30: 'dad', '4': 'kid', 19: 'teen', '100': 'grams'};
-// console.log(obj) // returns {4: "kid", 19: "teen", 30: "dad", 100: "grams"} with sorted keys
-
-// in ES6 let’s look at the easiest ways of removing duplicates from an array.
-// // considering a linear array Set gives us the answer we need.
-// const arr = [1, 2, 2, 4, 5, 5];
-// [...new Set(arr)]; // returns [1, 2, 4, 5]
-// // however set doesn't remove duplicates from array of objects
+// remove duplicates from array of objects
 // const arr = [{a:1},{b:2},{a:1}];
-// [...new Set(arr)]; // returns [{a:1},{b:2},{a:1}]
-// // hence we can use ES6 filter and map functions to achieve the same
-// arr.filter((obj, index) => {
+// let brr = arr.filter((obj, index) => {
+//   // return arr.map(obj['a']);
 //   return arr.map(obj => obj['a']).indexOf(obj['a']) === index;
 // }); // removes duplicate based on the key 'a'
+// console.log(brr);
 
 function callback (entries, observer) {
+  // if (甲) { if (乙) {A} else if (丙) {B} else {A} }
   entries.forEach(entry => {
-    console.log(this);
-      // if (entry.isIntersecting) {
-      //   entry.forEach((intersectionRatio, target) => {
-      //     ratio = this.intersectionRatio;
-      //     id = this.target.id;
-      //     //store [...{ratio,id}]
-      //     console.log([ratio, id]);
-      //
-      //   });
-      // }
+      if (entry.isIntersecting) { // 甲
+        if (data === []) { // 乙
+          // A
+          // obj = {[entry.target.id]: {ratio: entry.intersectionRatio}}; // 1st new target
+          // data.push(obj);
+        } else if (.hasOwnProperty(entry.target.id)) { // 丙 target exists
+            // B: for existing targets, update ratio only
+        } else { // A: a little WET, but it's ok.
+          // obj = {[entry.target.id]: {ratio: entry.intersectionRatio}}; // new target
+          // data.push(obj);
+        }
+    }
+    // console.log(obj[entry.target.id].ratio);
+    // obj[entry.target.id].ratio = entry.intersectionRatio;
+    // data.set(entry.target.id, {ratio: entry.intersectionRatio});
+    // console.log(entry.target.id);
+    // console.log(Object.keys(data[0])[0]);
+    // console.log(obj);
+    // console.log(data);
+    // entry.forEach((intersectionRatio, target) => {
+    //   ratio = this.intersectionRatio;
+    //   id = this.target.id;
+    //   //store [...{ratio,id}]
+    //   console.log([ratio, id]);
+    //
+    // });
   });
 } // end of callback
 
 // const btn = document.querySelector('button');
-// let dataEntry = {ratio:'', id:''};
-// let data = [];
+// let obj = {};
+let obj = {['']: {ratio:''}};
+let data = [];
+// let data = new Map();
+// console.log(Object.keys(obj)[0]); // ''
+// console.log(Object.values(obj)[0]); // {ratio:''}
 
 let observer = new IntersectionObserver(callback, options);
 // observer.observe(document.querySelector('#test-target'));
 document.querySelectorAll('.cs-cloudrone p').forEach(p => { observer.observe(p) });
+
 // console.log(observer);
 
 // var kvArray = [{key: 1, value: 10},
