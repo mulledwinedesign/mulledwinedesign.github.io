a0813d95150c63b72c2a16e40a983a7c5e071fbf
Xueer <xueerzhang@users.noreply.github.com>
2019-12-28 22:08:10 +0800

    implement whole logic – clean version

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 let obj = {['']: {ratio:''}};
-// console.log(Object.keys(obj)[0]); // ''
-// console.log(Object.values(obj)[0]); // {ratio:''}
 let data = [];
 
 function buildThresholdList (numSteps) {
   let thresholds = [];
-
   for (let i=1.0; i<=numSteps; i++) {
     let ratio = i/numSteps;
     thresholds.push(ratio);
   }
-
   thresholds.push(0); // [0.01, 0.02, ..., 1, 0]
-  // thresholds.push(0.000001); // [0.01, 0.02, ..., 1, 0.000001]
-
   return thresholds;
 }
 
@@ -24,35 +18,28 @@ function idExistsInData(id) {
 }
 
 const options = {
-  // root: null,
-  // rootMargin: '0px 0px -50% 0px',
   threshold: buildThresholdList(10)
 }
 
 function callback (entries, observer) {
-  // if (甲) { if (乙) {A} else if (丙) {B} else {A} }
   entries.forEach(entry => {
-      if (entry.isIntersecting) { // 甲
-        console.log(entry.target.id, entry.intersectionRatio);
-        obj = {[entry.target.id]: {ratio: entry.intersectionRatio}}; // current id n ratio
-        if (data.length === 0) { // 乙
-          data.push(obj); // A
-        } else if (idExistsInData(entry.target.id)) { // 丙 id exists
-          // B: update ratio only
-          for (const ele of data) {
-            if (Object.keys(ele)[0] === Object.keys(obj)[0]) {
-              Object.values(ele)[0].ratio = Object.values(obj)[0].ratio;
-            }
+    if (entry.isIntersecting) {
+      console.log(entry.target.id, entry.intersectionRatio);
+      obj = {[entry.target.id]: {ratio: entry.intersectionRatio}};
+      if (data.length === 0) {
+        data.push(obj);
+      } else if (idExistsInData(entry.target.id)) {
+        for (const ele of data) {
+          if (Object.keys(ele)[0] === Object.keys(obj)[0]) {
+            Object.values(ele)[0].ratio = Object.values(obj)[0].ratio;
           }
-          console.log(data);
-        } else { // for new id, A again. a little WET, but it's ok.
-          data.push(obj);
-          console.log(data); // debug tip: array keeps updating until – number shown – it's manually unfolded
         }
+      } else {
+        data.push(obj);
+      }
     }
   });
-} // end of callback
+}
 
 let observer = new IntersectionObserver(callback, options);
-// observer.observe(document.querySelector('#test-target'));
 document.querySelectorAll('.cs-cloudrone p').forEach(p => { observer.observe(p) });
