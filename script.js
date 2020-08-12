2ffe98eb05cc25e68ebc45b57252030132d952db
Xueer <xueerzhang@users.noreply.github.com>
2020-08-12 11:00:35 +0800

    debug coordinates

--- script.js
+++ script.js
@@ -35,15 +35,30 @@ let observer = new IntersectionObserver(callback, options);
 document.querySelectorAll('.cs-cloudrone p').forEach(p => { observer.observe(p) });
 
 
+function getViewportSize() {
+    let docEle = (document.compatMode && document.compatMode === "CSS1Compat") ? document.documentElement : document.body;
+    let w = docEle.clientWidth;
+    let h = docEle.clientHeight;
+    // mobile zoomed in?
+    if(window.innerWidth && w > window.innerWidth){
+      w = window.innerWidth;
+      h = window.innerHeight;
+    }
+    return {width: w, height: h};
+}
+
 let points = [];
+let pointsString = "";
 let dots2BConnected = document.getElementsByClassName("dots");
 for(item of dots2BConnected) {
-  let x = item.getBoundingClientRect().left;
-  let y = item.getBoundingClientRect().top + item.getBoundingClientRect().height/2;
+  let rect = item.getBoundingClientRect();
+  let x = rect.left;
+  let y = rect.top + item.clientHeight/2;
   points.push([x,y]);
-  console.log(item,points);
 }
-// let points = [{node.className:[x,y]}];
-// let pointsString = " ";
-let path = document.getElementById("flowpath");
-path.setAttribute("d","M"+points[0][0]+" "+points[0][1]+" h100"+"Z");
+points.forEach((item, i) => {
+  pointsString += "m"+points[i][0]+" "+points[i][1]+" v10 h10 v-10 h-10";
+});
+document.getElementById("flowpath").setAttribute("d",pointsString+"z");
+let svg = document.getElementsByClassName("flow")[0];
+svg.setAttribute("viewBox","0 0 "+getViewportSize().width+" "+getViewportSize().height);
