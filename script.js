9cb0f07f91606e19049005339b9c072ec41a7e1a
Xueer <xueerzhang@users.noreply.github.com>
2020-09-28 18:45:10 +0800

    recipe 0to1 line

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Intersection Observer
 let obj = {['']: {ratio:''}};
 let data = [];
 function buildThresholdList (numSteps) {
@@ -34,7 +35,7 @@ function callback (entries, observer) {
 let observer = new IntersectionObserver(callback, options);
 document.querySelectorAll('.cs-cloudrone p').forEach(p => { observer.observe(p) });
 
-
+// svg flow line
 function getViewportSize() {
   let docEle = (document.compatMode && document.compatMode === "CSS1Compat") ? document.documentElement : document.body;
   let w = docEle.clientWidth;
@@ -52,39 +53,83 @@ function updateViewBox() {
 }
 
 function connectDots() {
-  let p = [];
+  let d = [];
   let dots2BConnected = document.getElementsByClassName("dots");
   for(item of dots2BConnected) {
     let xLeft = item.getBoundingClientRect().left +window.pageXOffset;
     let xRight = item.getBoundingClientRect().left +window.pageXOffset +item.clientWidth;
     let y = item.getBoundingClientRect().top +window.pageYOffset +item.clientHeight/2;
-    p.push({xLeft:xLeft,xRight:xRight,y:y});
+    d.push({xLeft:xLeft,xRight:xRight,y:y});
   }
-  // console.log(p);
+  // console.log(d);
   document.getElementById("flowpath").setAttribute("d",
-    "M"+p[0].xLeft +" "+p[0].y+
-    "C"+p[0].xLeft+" "+(p[0].y+(p[1].y-p[0].y)*1.618)+","
-       +(p[1].xRight+(p[0].xLeft-p[1].xRight)*0.618)+" "+p[1].y+","
-       +p[1].xRight+" "+p[1].y+
-    "M"+p[1].xLeft +" "+p[1].y+"h-20"+
-    "C"+p[1].xLeft+" "+(p[1].y+(p[2].y-p[1].y)*0.618/2)+","
-       +p[1].xLeft*0.618+" "+(p[1].y+(p[2].y-p[1].y)*0.618)+","
-       +p[2].xLeft +" "+p[2].y+
-    "M"+p[2].xRight+" "+p[2].y+
-    "C"+(p[2].xRight+(p[3].xLeft-p[2].xRight)*0.3)+" "+p[2].y*0.95+","
-       +(p[2].xRight+(p[3].xLeft-p[2].xRight)*0.6)+" "+p[2].y*1.01+","
-       +p[3].xLeft +" "+p[3].y+
-    "M"+p[3].xRight+" "+p[3].y+
-    "L"+p[4].xRight+" "+p[4].y+
-    "M"+p[4].xLeft +" "+p[4].y+
-    "L"+p[5].xLeft +" "+p[5].y+
-    "M"+p[5].xRight+" "+p[5].y+
-    "L"+p[6].xLeft +" "+p[6].y+
-    "M"+p[6].xRight+" "+p[6].y+
-    "L"+p[7].xLeft +" "+p[7].y);
+    "M"+d[0].xLeft +" "+d[0].y+
+    "C"+d[0].xLeft+" "+(d[0].y+(d[1].y-d[0].y)*1.618)+","
+       +(d[1].xRight+(d[0].xLeft-d[1].xRight)*0.618)+" "+d[1].y+","
+       +d[1].xRight+" "+d[1].y+
+    "M"+d[1].xLeft +" "+d[1].y+"h-20"+
+    "C"+d[1].xLeft+" "+(d[1].y+(d[2].y-d[1].y)*0.618/2)+","
+       +d[1].xLeft*0.618+" "+(d[1].y+(d[2].y-d[1].y)*0.618)+","
+       +d[2].xLeft +" "+d[2].y+
+    "M"+d[2].xRight+" "+d[2].y+
+    "C"+(d[2].xRight+(d[3].xLeft-d[2].xRight)*0.3)+" "+d[2].y*0.95+","
+       +(d[2].xRight+(d[3].xLeft-d[2].xRight)*0.6)+" "+d[2].y*1.01+","
+       +d[3].xLeft +" "+d[3].y+
+    "M"+d[3].xRight+" "+d[3].y+
+    "L"+d[4].xRight+" "+d[4].y+
+    "M"+d[4].xLeft +" "+d[4].y+
+    "L"+d[5].xLeft +" "+d[5].y+
+    "M"+d[5].xRight+" "+d[5].y+
+    "L"+d[6].xLeft +" "+d[6].y+
+    "M"+d[6].xRight+" "+d[6].y+
+    "L"+d[7].xLeft +" "+d[7].y
+  );
 }
 
 updateViewBox();
 window.addEventListener("load", connectDots);
 window.addEventListener("resize", updateViewBox);
 window.addEventListener("resize", connectDots);
+
+// svg recipe line: from 0 to 1
+function draw0to1() {
+  // get recipeBox size
+
+  // update recipeBox viewBox
+  document.getElementsByClassName("0to1")[0].setAttribute("viewBox","0 0 "+recipeBox.width+" "+recipeBox.height);
+
+  // collect 0to1dots coordinates
+  let d = [];
+  let dots2BConnected = document.getElementsByClassName("0to1dots");
+  for(item of dots2BConnected) {
+    let xLeft = item.getBoundingClientRect().left +window.pageXOffset;
+    let xRight = item.getBoundingClientRect().left +window.pageXOffset +item.clientWidth;
+    let y = item.getBoundingClientRect().top +window.pageYOffset +item.clientHeight/2;
+    d.push({xLeft:xLeft,xRight:xRight,y:y});
+  }
+
+  // connect 0to1dots to draw the line
+  document.getElementById("0").setAttribute("cx",);
+  document.getElementById("0").setAttribute("cy",);
+  document.getElementById("0").setAttribute("r",);
+  document.getElementById("0to1path").setAttribute("d",
+    "M"+(d[0].xLeft-(gap))+" "+d[0].y+
+    "H"+d[0].xLeft+" "+
+    "M"+d[0].xRight+" "+d[0].y+
+    "H"+d[1].xLeft+" "+
+    "M"+d[1].xRight+" "+d[0].y+
+    "H"+d[2].xLeft+" "+
+    "M"+d[2].xRight+" "+d[0].y+
+    "H"+d[3].xLeft+" "+
+    "M"+d[3].xRight+" "+d[0].y+
+    // "C"+  d[4].xLeft+" "+
+    "M"+d[4].xRight+" "+d[0].y+
+    "h"+(gap)
+  );
+  document.getElementById("1").setAttribute("x1",);
+  document.getElementById("1").setAttribute("y1",);
+  document.getElementById("1").setAttribute("x2",);
+  document.getElementById("1").setAttribute("y2",);
+}
+window.addEventListener("load", draw0to1);
+window.addEventListener("resize", draw0to1);
